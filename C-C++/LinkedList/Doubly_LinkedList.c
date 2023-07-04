9d2f377a5504c851660a6f6d52aa09a183497a67
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-07-04 09:52:54 +0100

    added a function to add node to end of DLL

--- C-C++/LinkedList/Doubly_LinkedList.c
+++ C-C++/LinkedList/Doubly_LinkedList.c
@@ -22,11 +22,34 @@ struct node *addToEmpty(struct node *head, int data) {
     return head; // return address to node
 }
 
+
+//Function to insert at the end of Linkedlist
+struct node *addToend(struct node *head, int data) {
+
+    struct node *tp = head;
+    struct node *temp = (struct node *)malloc(sizeof(struct node));
+    temp -> prev = NULL;
+    temp -> data = data;
+    temp -> next = NULL;
+
+    while (tp -> next != NULL) {
+
+        tp = tp -> next;
+
+    }
+    tp -> next = temp;
+    temp->prev = tp;
+    return head;
+     
+}
+
 // Creating node of the Doubly linkedlist
 int main()
 {
     struct node *head = NULL;
     head = addToEmpty(head, 45);
     printf("%d\n", head->data);
+    head = addToend(head, 10);
+    printf("%d\n", head->next->data);
     return 0;
 }
